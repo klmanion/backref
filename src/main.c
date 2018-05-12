905220ec09572eca030c44e0b242a73ff0268e71
Kurt L. Manion <klmanion@gmail.com>
2018-05-12 12:52:07 -0400

    added src/main.c

---
+++ src/main.c
@@ -0,0 +1,19 @@
+//main.c
+// created by: Kurt L. Manion
+// on: Sat., 12 May 2018
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int
+main(
+	int argc,
+	char *const argv[])
+{
+	puts("foo");
+
+	return EXIT_SUCCESS;
+}
+
+/* vim: set ts=4 sw=4 noexpandtab tw=79: */
